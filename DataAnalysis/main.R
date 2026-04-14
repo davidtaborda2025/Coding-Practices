df474c075b575cfee035fecb6286db540c7a40e1
davidtaborda2025 <taborda.david@correounivalle.edu.co>
2026-04-13 22:24:35 -0500

    Mejor implementación de código en R, pudiendo generar una gráfica sobre los intentos de login, para auditoría.

--- DataAnalysis/main.R
+++ DataAnalysis/main.R
@@ -1,5 +1,6 @@
 library(DBI)
 library(RPostgres)
+library(ggplot2)
 
 con <- dbConnect(RPostgres::Postgres(),
                  dbname = 'test_db', host = 'localhost', port = 5432,
@@ -7,8 +8,14 @@ con <- dbConnect(RPostgres::Postgres(),
                  password = 'password123')
 
 datos_auditoria <- dbReadTable(con, "login_auditory")
+dbDisconnect(con)
+
+grafica <- ggplot(datos_auditoria, aes(x = state, fill = state)) + geom_bar() +
+  labs(title = "Analisis de Intentos de Login", subtitle = "Datos obtenidos en tiempo real desde PostgreSQL",
+       x = "Estado del Intento", y = "Cantidad de Registros") + theme_minimal() +
+  scale_fill_manual(values = c("FALLO" = "#e74c3c", "EXITO" = "#2ecc71"))
 
 print("--- DATOS RECUPERADOS DESDE DOCKER ---")
 print(datos_auditoria)
 
-dbDisconnect(con)
+print(grafica)
