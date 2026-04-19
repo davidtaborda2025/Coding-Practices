3e49f3a94a72a241e9f9e50841cd32c0bef460f3
davidtaborda2025 <taborda.david@correounivalle.edu.co>
2026-04-19 00:39:46 -0500

    Mejoras a la gráfica.

--- DataAnalysis/main.R
+++ DataAnalysis/main.R
@@ -35,11 +35,14 @@ if (db_url != "") {
 datos_auditoria <- dbReadTable(con, "login_auditory")
 dbDisconnect(con)
 
+conteo <- table(datos_auditoria$state)
+max_registros <- if(length(conteo) > 0) max(conteo) else 1
+
 grafica <- ggplot(datos_auditoria, aes(x = state, fill = state)) + geom_bar(width = 0.6) +
   labs(title = "Analisis de Intentos de Login", subtitle = "Datos obtenidos en tiempo real desde PostgreSQL",
        x = "Estado del Intento", y = "Cantidad de Registros") + theme_minimal() +
   scale_fill_manual(values = c("FALLO" = "#e74c3c", "EXITO" = "#2ecc71")) +
-  scale_y_continuous(labels = label_number(accuracy = 1), breaks = pretty_breaks()) +
+  scale_y_continuous(breaks = seq(0, max_registros, by = 1)) +
   theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
         plot.subtitle = element_text(size = 12, hjust = 0.5), plot.margin = margin(20, 40, 20, 20),
         axis.title.x = element_text(margin = margin(t = 15)), axis.title.y = element_text(margin = margin(r = 15)),
